5e18e3bd672d817cc54f04757ce6914a604ee8cb
soleilyasmina <mounlasolomon@gmail.com>
2019-10-06 15:25:08 -0400

    made route more specific

--- solello-api/config/routes.rb
+++ solello-api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :columns
-  put '/columns/:id/:new_order', to: 'columns#swap'
+  put '/columns/:id/:new_order', to: 'columns#swap_columns'
   resources :boards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
