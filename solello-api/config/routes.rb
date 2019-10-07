e1753666ce2efd9dbc65711790336becc7eb7330
soleilyasmina <mounlasolomon@gmail.com>
2019-10-06 21:38:58 -0400

    reorganized routes

--- solello-api/config/routes.rb
+++ solello-api/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
+  resources :boards
   resources :columns
   put '/columns/:id/:new_order', to: 'columns#swap_columns'
-  resources :boards
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
